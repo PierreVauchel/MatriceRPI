eeed750d1bdfae01e366327a94ecfda0e738669b
Pierre Vauchel <PierreVauchel@users.noreply.github.com>
2023-05-09 23:01:40 +0200

    Ajout msg.txt

--- formulaire.php
+++ formulaire.php
@@ -3,9 +3,10 @@
 if (isset($_POST['modifMatrice'])) {
     $msg = $_POST['msg'];
 
-    $file = fopen('msg.txt', 'a');
-    fwrite($file , $content);
+    $file = fopen('msg.txt', 'w');
+    fwrite($file , $msg);
     fclose($file);
+    header("location:congrats.html");
     exit();
 }
 ?>
